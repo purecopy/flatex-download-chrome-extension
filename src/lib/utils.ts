46c972571d0fe160cf7d448342cd4d4c3bd92d5d
Jonathan Heneis <jh@cognify.ai>
2022-04-10 22:22:55 +0200

    feat: adjust error level

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -64,7 +64,7 @@ export async function asyncMapSerial<T, U>(
         const value = await fn(cur, index);
         return [...prev, value];
       } catch (e) {
-        console.error(e);
+        console.warn(e);
 
         if (retry && retryCount < maxRetry) {
           await sleep(1000);
