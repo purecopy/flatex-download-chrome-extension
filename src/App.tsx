19c96e534f8cc85dbca5135531b7ca87b46137cc
Jonathan Heneis <jh@cognify.ai>
2022-04-10 20:42:50 +0200

    feat: add downloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from './Button';
+import { useDownload } from './useDownload';
 
 function App() {
+  const { count, isLoading, downloadAll } = useDownload();
+
+  function handleClickDownload() {
+    downloadAll()
+      .then(({ count }) => alert(`Download completed (${count} documents)`))
+      .catch(() => alert('Download failed :('));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button className="App-download-button" loading={isLoading} onClick={handleClickDownload} disabled={count <= 0}>
+          Download
+        </Button>
+        <p>{count} Documents found</p>
       </header>
     </div>
   );
