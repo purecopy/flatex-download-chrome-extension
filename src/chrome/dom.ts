7487063af100da126e070bfe817eb69c22711da9
Jonathan Heneis <jona.heneis@gmail.com>
2023-10-22 17:45:33 +0200

    feat: add simple loading spinner

--- src/chrome/dom.ts
+++ src/chrome/dom.ts
@@ -25,6 +25,53 @@ function getCredentialsWithCacheFn() {
 
 const getCredentialsWithCache = getCredentialsWithCacheFn();
 
+async function getPdfWithVerification(docLink: string) {
+  try {
+    return await getPdf(docLink);
+  } catch (error) {
+    if (error instanceof Response && error.status === 503) {
+      await verifyPdfLink(docLink);
+      return await getPdf(docLink);
+    }
+
+    throw error;
+  }
+}
+
+function toggleLoader(show: boolean) {
+  const prevLoader = document.getElementById('flatex-downloader-loader');
+
+  // create html element
+  if (show && !prevLoader) {
+    const loader = document.createElement('div');
+    loader.id = 'flatex-downloader-loader';
+    loader.style.position = 'fixed';
+    loader.style.bottom = '32px';
+    loader.style.left = '32px';
+    loader.style.backgroundColor = '#282c34';
+    loader.style.zIndex = '99999';
+    loader.style.padding = '12px 32px';
+    loader.style.color = '#90caf9';
+
+    const headline = document.createElement('div');
+    headline.innerText = 'Flatex Downloader (Community Edition)';
+    headline.style.fontSize = '12px';
+    headline.style.marginBottom = '6px';
+
+    const subHeadline = document.createElement('div');
+    subHeadline.innerText = 'Preparing Download...';
+    subHeadline.style.fontSize = '24px';
+    subHeadline.style.fontWeight = 'bold';
+
+    loader.appendChild(headline);
+    loader.appendChild(subHeadline);
+
+    document.body.appendChild(loader);
+  } else if (!show && prevLoader) {
+    prevLoader.remove();
+  }
+}
+
 const handleMessages = (
   msg: DOMMessage,
   _sender: chrome.runtime.MessageSender,
@@ -42,26 +89,17 @@ const handleMessages = (
     case 'POST_DOWNLOAD':
       const rows = getDocumentRows();
       const data = getFormData();
+      toggleLoader(true);
 
       // TODO: Improve error handling
       // - Display failed downloads
       getCredentialsWithCache()
         .then((credentials) =>
-          asyncMapSerial(rows, (row) => withRetry(() => getDocumentLink(data, row, { credentials }))),
-        )
-        .then((docLinks) =>
-          asyncMapSerial(docLinks, async (docLink) => {
-            try {
-              return await getPdf(docLink);
-            } catch (error) {
-              if (error instanceof Response && error.status === 503) {
-                await verifyPdfLink(docLink);
-                return await getPdf(docLink);
-              }
-
-              throw error;
-            }
-          }),
+          asyncMapSerial(rows, (row) =>
+            withRetry(() => getDocumentLink(data, row, { credentials })).then((docLink) =>
+              withRetry(() => getPdfWithVerification(docLink)),
+            ),
+          ),
         )
         .then((pdfs) => createZip(pdfs))
         .then((blob) => saveAs(blob, `flatex-downloader-export.zip`))
@@ -71,6 +109,9 @@ const handleMessages = (
         .catch((error) => {
           console.log(error);
           sendResponse({ success: false, count: 0, reason: error.message });
+        })
+        .finally(() => {
+          toggleLoader(false);
         });
 
       // keep message channel open
